6ce2927b24c1ff6a7e1eaa60424c3fa6acd876c4
batikansavci <61455378+batikansavci@users.noreply.github.com>
2022-04-08 20:32:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function changeImage(img) {
     document.getElementById("changeImg").src = img.src.replace("_1", "_0");
 }
+$('#cv_print').click(
+    function () {
+        var w = window.open();
+        var header= $("#print_general").html();
+        
+
+        var html = "<!DOCTYPE HTML>";
+        html += '<html lang="en-us">';
+        html += '<head><style></style></head>';
+        html += "<body>";
+        
+        if(header != null) html += header + "<br/><br/>";
+        
+        html += "</body>";
+        w.document.write(html);
+        w.window.print();
+        w.document.close();
+        w.close();
+    }
+);
