f60823c8bc31e152689400fd72577c0e838bf538
batikansavci <61455378+batikansavci@users.noreply.github.com>
2022-04-08 20:54:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ function changeImage(img) {
 $('#cv_print').click(
     function () {
         var w = window.open();
-        var header= $("#print_general").html();
-        
-
+        var header= $("#print_general").html();       
         var html = "<!DOCTYPE HTML>";
         html += '<html lang="en-us">';
         html += '<head><style></style></head>';
-        html += "<body>";
-        
-        if(header != null) html += header + "<br/><br/>";
-        
+        html += "<body>";    
+        if(header != null) html += header + "<br/><br/>";  
         html += "</body>";
         w.document.write(html);
         w.window.print();
